b806ab0ab4f7eb14721ed287af6c50b22149116f
Fulei Huang <huangfulei1027@gmail.com>
2021-01-23 16:26:51 +0000

    added actions and credentials

--- actions/actions.py
+++ actions/actions.py
@@ -11,10 +11,9 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormAction
+from rasa_sdk.forms import FormValidationAction
 
-#
-#
+
 # class ActionHelloWorld(Action):
 #
 #     def name(self) -> Text:
@@ -27,3 +26,41 @@ from rasa_sdk.forms import FormAction
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+#
+# class ValidateOrderForm(FormValidationAction):
+#     def name(self) -> Text:
+#         return "validate_order_form"
+#
+#     async def validate_email(
+#             self,
+#             value: Text,
+#             dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any],
+#     ) -> Dict[Text, Any]:
+#         if value:
+#             print('this is the value', value)
+#             return {"confirm_exercise": True}
+#         else:
+#             print('this is the value: ', value)
+#             return {"exercise": "None", "confirm_exercise": False}
+#
+
+class ActionSubmitResults(Action):
+    def name(self) -> Text:
+        return "action_submit_results"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        email = tracker.get_slot("email")
+        payment_method = tracker.get_slot("payment_method")
+
+        print('email is: ', email)
+        print('payment method is: ', payment_method)
+
+        dispatcher.utter_message("Thanks, your order has been placed!")
+        return []
